efa5bd0d67b36413b2c5022fa18a68b3295b69d8
cafalchio <maolivei@tcd.ie>
2023-08-07 13:56:44 +0100

    fix menu mobile

--- api_backend/admin.py
+++ api_backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from api_backend.models import CryptoCurrency
+from api_backend.models import  CryptoCurrency
 
 # Register your models here.
 admin.site.register(CryptoCurrency)
+
--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -169,11 +169,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-# STATICFILES_IGNORE_PATTERNS = [
-#     r"browser-sync/browser-sync-client\.js$",
-# ]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'theme', 'static'),
