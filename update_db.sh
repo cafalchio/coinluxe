235562ba2b6803e4286f175774b1240d3d135cdd
cafalchio <maolivei@tcd.ie>
2023-09-14 23:06:05 +0100

    added command to update_db script

--- update_db.sh
+++ update_db.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Navigate to the directory containing your Python script
 cd /home/cafalchio/Projects/coinluxe
 source .venv/bin/activate
 
-# Run the Python command
 python manage.py update_coins
 python manage.py update_coins_details
 python manage.py update_coins_charts
-python manage.py update_prices
 python manage.py update_coins --save_pics True
+python manage.py create_stripe_products
