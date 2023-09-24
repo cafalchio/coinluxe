c0e9d88129c495cd0e4b2e4d2222692179480c76
cafalchio <maolivei@tcd.ie>
2023-09-24 13:20:50 +0100

    returned commented functions

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -26,77 +26,73 @@ def get_debit(request):
 
 @login_required(login_url="login")
 def add_debit(request):
-    pass
-    # stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
-    # metadata = {"user_id": str(request.user.id)}
-    # if request.method == "POST":
-    #     checkout_session = stripe.checkout.Session.create(
-    #         payment_method_types=["card"],
-    #         line_items=[
-    #             {
-    #                 "price": settings.PRODUCT_PRICE,
-    #                 "quantity": 1,
-    #             },
-    #         ],
-    #         mode="payment",
-    #         customer_creation="always",
-    #         success_url=settings.REDIRECT_DOMAIN
-    #         + "/payment_successful?session_id={CHECKOUT_SESSION_ID}",
-    #         cancel_url=settings.REDIRECT_DOMAIN + "/payment_cancelled",
-    #         metadata=metadata,
-    #     )
-    #     return redirect(checkout_session.url, code=303)
-    # return render(request, "shopping_bag/bag.html")
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    metadata = {"user_id": str(request.user.id)}
+    if request.method == "POST":
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[
+                {
+                    "price": settings.PRODUCT_PRICE,
+                    "quantity": 1,
+                },
+            ],
+            mode="payment",
+            customer_creation="always",
+            success_url=settings.REDIRECT_DOMAIN
+            + "/payment_successful?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=settings.REDIRECT_DOMAIN + "/payment_cancelled",
+            metadata=metadata,
+        )
+        return redirect(checkout_session.url, code=303)
+    return render(request, "shopping_bag/bag.html")
 
 
 def payment_successful(request):
-    pass
-    # stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
-    # checkout_session_id = request.GET.get("session_id", None)
-    # session = stripe.checkout.Session.retrieve(checkout_session_id)
-    # customer = stripe.Customer.retrieve(session.customer)
-    # if settings.DEBUG:
-    #     print(f"{session} -> Session ID")
-    #     print(f"{customer} -> costumer ")
-    # return render(request,
-    #               "shopping_bag/payment_successful.html",
-    #               {"customer": customer})
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    checkout_session_id = request.GET.get("session_id", None)
+    session = stripe.checkout.Session.retrieve(checkout_session_id)
+    customer = stripe.Customer.retrieve(session.customer)
+    if settings.DEBUG:
+        print(f"{session} -> Session ID")
+        print(f"{customer} -> costumer ")
+    return render(request,
+                  "shopping_bag/payment_successful.html",
+                  {"customer": customer})
 
 
 def payment_cancelled(request):
-    pass
-    # stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
-    # return render(request, "shopping_bag/payment_cancelled.html")
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    return render(request, "shopping_bag/payment_cancelled.html")
 
 
 @csrf_exempt
 def stripe_webhook(request):
-    pass
-    # stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
-    # time.sleep(6)
-    # payload = request.body
-    # signature_header = request.META["HTTP_STRIPE_SIGNATURE"]
-    # event = None
-    # try:
-    #     event = stripe.Webhook.construct_event(
-    #         payload, signature_header, settings.STRIPE_WEBHOOK_SECRET_TEST
-    #     )
-    # except ValueError:
-    #     return HttpResponse(status=400)
-    # except stripe.error.SignatureVerificationError:
-    #     return HttpResponse(status=400)
+    stripe.api_key = settings.STRIPE_SECRET_KEY_TEST
+    time.sleep(6)
+    payload = request.body
+    signature_header = request.META["HTTP_STRIPE_SIGNATURE"]
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, signature_header, settings.STRIPE_WEBHOOK_SECRET_TEST
+        )
+    except ValueError:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        return HttpResponse(status=400)
 
-    # if event["type"] == "checkout.session.completed":
-    #     session = event["data"]["object"]
-    #     session_id = session.get("id", None)
-    #     time.sleep(1)
-    #     line_items = stripe.checkout.Session.list_line_items(session_id,
-    #                                                          limit=1)
-    #     item = line_items.data[0]
-    #     value = int(item.amount_total) / 100
-    #     debit.amount += Decimal(value)
-    #     debit.save()
-    # return HttpResponse(status=200)
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        session_id = session.get("id", None)
+        time.sleep(1)
+        line_items = stripe.checkout.Session.list_line_items(session_id,
+                                                             limit=1)
+        item = line_items.data[0]
+        value = int(item.amount_total) / 100
+        debit.amount += Decimal(value)
+        debit.save()
+    return HttpResponse(status=200)
 
 
 @login_required(login_url="account_login")
