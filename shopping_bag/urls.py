ad0a35c64b39333b1a430af26bf5e3afa221576e
cafalchio <maolivei@tcd.ie>
2023-09-30 13:31:18 +0100

    create wallet view

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_to_bag, get_debit, pay, payment_cancelled, payment_successful, remove_crypto, shopping_bag_view
+from .views import add_to_bag, get_debit, pay, payment_cancelled, payment_successful, remove_crypto, shopping_bag_view, wallet_view
 
 urlpatterns = [
     path("get-debit/", get_debit, name="get_debit"),
@@ -8,5 +8,6 @@ urlpatterns = [
     path("pay/", pay, name="pay"),
     path("add_to_bag/<str:pk>/", add_to_bag, name="add_to_bag"),
     path("sell-crypto/<str:pk>/", remove_crypto, name="sell_crypto"),
-    path("", shopping_bag_view, name="bag")
+    path("", shopping_bag_view, name="bag"),
+    path("wallet", wallet_view, name="wallet")
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -132,3 +132,25 @@ def shopping_bag_view(request):
         })
     context = {'crypto_data': crypto_data}
     return render(request, template_name, context)
+
+
+@login_required(login_url="account_login")
+def wallet_view(request):
+    template_name = "shopping_bag/wallet.html"
+    user = request.user
+    shopping_bag, _ = Bag.objects.get_or_create(owner=user)
+    crypto_data = []
+    holdings = Holding.objects.filter(shopping_bag=shopping_bag)
+    for holding in holdings:
+        if holding.amount <= 0:
+            continue
+        value = holding.cryptocurrency.current_price
+        value_eur = f"{holding.amount * value:.2f} €"
+        crypto_data.append({
+            'crypto': holding.cryptocurrency,
+            'amount': holding.amount,
+            'f_amount': holding.formatted_amount,
+            'value': value_eur,
+        })
+    context = {'crypto_data': crypto_data}
+    return render(request, template_name, context)
