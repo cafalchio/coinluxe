f38aa01e2718cd6038e2665c7f9273b3c013a45a
cafalchio <maolivei@tcd.ie>
2023-10-01 11:17:24 +0100

    added min payment value

--- theme/static/script.js
+++ theme/static/script.js
@@ -26,6 +26,7 @@ function validateInput() {
 function checkValue(e) {
   // Check stripe max value and message user
   const maxValue = 999999.99
+  const minValue = 0.04
   let total = 0;
   const values = document.querySelectorAll(".cvalue");
 
@@ -37,6 +38,10 @@ function checkValue(e) {
       e.preventDefault()
       errorMessage.textContent = "Value too high to be processed here. Please contact us at webcoinluxe@gmail.com";
       return false; 
+  } else if (total < minValue) {
+    e.preventDefault()
+    errorMessage.textContent = "Value too low to be paid, please add more cryptos to your bag.";
+    return false; 
   } else {
       errorMessage.textContent = "";
       return true; 
